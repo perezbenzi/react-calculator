6609df2a7e7e80121bb67ab8c003395b706e9b0f
Francisco Perez <perezbenzif@gmail.com>
2022-09-14 20:40:32 -0300

    firsts styles

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import styles from "../css/display.module.css";
 
 export const Display = () => {
-  return (
-    <div>Display</div>
-  )
-}
+    return (
+        <div className={styles.display}>
+            <div className={styles.mode}>
+                <span className={styles.containerBtn}>
+                    <button className={`${styles.modeBtn} ${styles.modeLight}`}>
+                        L
+                    </button>
+                    <button className={`${styles.modeBtn} ${styles.modeDark}`}>
+                        D
+                    </button>
+                </span>
+            </div>
+            <div className={styles.operation}>
+                1<span className={styles.operador}> + </span>1
+            </div>
+            <div className={styles.result}>2</div>
+        </div>
+    );
+};
